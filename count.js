f8199c8fdbe731b5d6b816d012e4c3c522a85c19
jaysonhusky <jaysonhusky@live.com>
2019-09-16 17:48:51 +0100

    Good Commentary

--- count.js
+++ count.js
@@ -1,30 +1,36 @@
 function addCount(e) {
-	var id=e.getAttribute('data-lc');
-	var post='id='+id;
-	var req=new XMLHttpRequest();
+	var id=e.getAttribute('data-lc'); // Leave this as it is. The script knows to look for this attribute to count the clicks!
+	var post='id='+id; 
+	var req=new XMLHttpRequest(); // Assembles the data and begins to pass it along to the initCount.php script.
 	req.open('POST','/initCount.php',true);
 	req.setRequestHeader('Content-Type','application/x-www-form-urlencoded');
 	req.onreadystatechange=function(){
+		// Checking for an OK from the server before returning data, if it fails, no data is returned.
             if(req.readyState !== 4 || req.status !== 200) return;
             document.getElementById("clicks").innerHTML=req.responseText;
 	 };
+	 // Send data to the script.
 	req.send(post);
-	//e.preventDefault();
 };
 
 function countCheck(){
-	var file = "js/click_data.txt";
+	var file = "js/click_data.txt"; // Locate file
     var rf=new XMLHttpRequest();
-    rf.open("GET", file, false);
+    rf.open("GET", file, false); // Open the file and read contents.
     rf.onreadystatechange=function(){
         if(rf.readyState===4){
 			if(rf.status===200||rf.status==0){
+				// If the server responds ok, then read the contents out into the page.
                 var at=rf.responseText;
                 
+				// Update text of "clicks"
 				document.getElementById("clicks").innerText=at;
             }
         }
     }
+	// We're sending no data back, so NULL is set!
     rf.send(null);
 }
+
+// Force a check on the page load.
 window.onload = countCheck;
--- initCount.php
+++ initCount.php
@@ -3,7 +3,7 @@
 $file = 'click_data.txt';
 //Open the file in read write mode
 $fh = fopen($file, 'r+');
-//Assign the page referene posted from page to a variable
+//Assign the page reference posted from page to a variable
 $id = $_REQUEST['id'];
 //Initialize lines variable
 $lines = '';
@@ -11,8 +11,10 @@ $lines = '';
 while(!feof($fh)){
     //Split the line using comma delimiter
     $row = explode(',', fgets($fh));
-    $pageref = trim($row[0]);
-    $count = trim($row[1]);
+	// We're not updating "$row[0]" as this is the name of the variable.
+	$pageref = trim($row[0]); 
+	// Identify number and check to increment it
+    $count = trim($row[1]); 
     if(!empty($pageref)){
     	if($pageref == $id){
     	 //Increment count
@@ -23,6 +25,7 @@ while(!feof($fh)){
     	$lines .= "$pageref,$count\r\n";
     }
 }
+// Update file and close
 file_put_contents($file, $lines);
 fclose($fh);
 ?>
